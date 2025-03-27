84201255e406f270fbe453537509f3517432fe87
Naubaj <naubadj@gmail.com>
2025-03-27 20:26:24 +1100

    redirect to user's dashboard

---
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -0,0 +1,36 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) {
+  const { userId } = await auth();
+  // asynchronous access of `params.id`.
+  const { storeId } = await params;
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: storeId,
+      userId,
+    },
+  });
+
+  if (!store) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <div> NavBar </div>
+      {children}
+    </>
+  );
+}
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -41,8 +41,8 @@ export const StoreModal = () => {
       setLoading(true);
 
       const response = await axios.post("/api/stores", values);
-      toast.success("Store created.");
       // Redirect
+      window.location.assign(`/${response.data.id}`);
 
       console.log(response.data);
     } catch (error) {
