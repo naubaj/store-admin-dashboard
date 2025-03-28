787da8da873df74e0a7ce6924962f49af76708e2
Naubaj <naubadj@gmail.com>
2025-03-29 02:27:21 +1100

    update routes to enable public api calls

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)"]);
-
+const isPublicRoute = createRouteMatcher([
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/api/:path*",
+]);
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
     await auth.protect();
