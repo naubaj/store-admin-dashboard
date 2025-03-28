7530d7b2727e8bfc9741127c7bf670ba918c9b40
Naubaj <naubadj@gmail.com>
2025-03-28 22:35:05 +1100

    uploads billboard

--- README.md
+++ README.md
@@ -34,3 +34,9 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+# Credits
+
+## Images used
+
+Photo by <a href="https://unsplash.com/@codioful?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Codioful (Formerly Gradienta)</a> on <a href="https://unsplash.com/photos/blue-and-yellow-digital-wallpaper-QWutu2BRpOs?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
---
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -0,0 +1,113 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  _req: Request,
+  { params }: { params: { billbordId: string } }
+) {
+  try {
+    const { billbordId } = await params;
+
+    if (!billbordId) {
+      return new NextResponse("Billboard ID is required", { status: 400 });
+    }
+
+    const billboard = await prismadb.billboard.findUnique({
+      where: { id: billbordId },
+    });
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_GET]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; billboardId: string } }
+) {
+  try {
+    const { userId } = await auth();
+    const { storeId, billboardId } = await params;
+    const body = await req.json();
+
+    const { label, imageUrl } = body;
+
+    if (!userId) {
+      return new NextResponse("Unathenticated", { status: 401 });
+    }
+
+    if (!label) {
+      return new NextResponse("Label is required", { status: 400 });
+    }
+
+    if (!imageUrl) {
+      return new NextResponse("Image URL is required", { status: 400 });
+    }
+
+    if (!billboardId) {
+      return new NextResponse("Billboard id is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorised", { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.updateMany({
+      where: { id: billboardId },
+      data: {
+        label,
+        imageUrl,
+      },
+    });
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_PATCH]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
+
+export async function DELETE(
+  _req: Request,
+  { params }: { params: { storeId: string; billbordId: string } }
+) {
+  try {
+    const { userId } = await auth();
+    const { storeId, billbordId } = await params;
+
+    if (!userId) {
+      return new NextResponse("Unathenticated", { status: 401 });
+    }
+
+    if (!billbordId) {
+      return new NextResponse("Billboard ID is required", { status: 400 });
+    }
+
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorised", { status: 403 });
+    }
+
+    const billboard = await prismadb.billboard.deleteMany({
+      where: { id: billbordId },
+    });
+    return NextResponse.json(billboard);
+  } catch (error) {
+    console.log("[BILLBOARD_DELETE]", error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+}
