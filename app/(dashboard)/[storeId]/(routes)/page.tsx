28e505c1e75492e8919bebf86a98fe2b37dbcb6e
Naubaj <naubadj@gmail.com>
2025-04-01 20:23:17 +1100

    dashboard page (render statistics for sales, revenue)

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,15 +1,83 @@
-import prismadb from "@/lib/prismadb";
+import { CreditCardIcon, DollarSign, PackageIcon } from "lucide-react";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { formatter } from "@/lib/utils";
+import { getTotalRevenue } from "@/actions/get-total-revenue";
+import { getSalesCount } from "@/actions/get-sales-count";
+import { getStockCount } from "@/actions/get-stock-count";
+import { Overview } from "@/components/overview";
+import { getGraphRevenue } from "@/actions/get-graph-revenue";
+
+// import prismadb from "@/lib/prismadb";
 
 interface DashboardPageProps {
   params: { storeId: string };
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
+  // const { storeId } = await params;
+  // const store = await prismadb.store.findFirst({
+  //   where: { id: storeId },
+  // });
   const { storeId } = await params;
-  const store = await prismadb.store.findFirst({
-    where: { id: storeId },
-  });
-  return <div>Active Store: {store?.name}</div>;
+  const totalRevenue = await getTotalRevenue(storeId);
+  const salesCount = await getSalesCount(storeId);
+  const stockCount = await getStockCount(storeId);
+  const graphRevenue = await getGraphRevenue(storeId);
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <Heading title="Dashboard" description="Overview of your store" />
+        <Separator />
+        <div className="grid gap-4 grid-cols-3">
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Total Revenue
+              </CardTitle>
+              <DollarSign className="h-4 w-4 text-muted-foreground" />
+              <CardContent>
+                <div className="text-2xl font-bold">
+                  {formatter.format(totalRevenue)}
+                </div>
+              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">Sales</CardTitle>
+              <CreditCardIcon className="h-4 w-4 text-muted-foreground" />
+              <CardContent>
+                <div className="text-2xl font-bold">+{salesCount}</div>
+              </CardContent>
+            </CardHeader>
+          </Card>
+          <Card>
+            <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+              <CardTitle className="text-sm font-medium">
+                Products In Stock
+              </CardTitle>
+              <PackageIcon className="h-4 w-4 text-muted-foreground" />
+              <CardContent>
+                <div className="text-2xl font-bold">{stockCount}</div>
+              </CardContent>
+            </CardHeader>
+          </Card>
+        </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle>Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
